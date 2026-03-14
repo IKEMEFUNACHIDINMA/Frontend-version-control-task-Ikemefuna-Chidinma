1dc4f58efadab482dd74dc7a6adc7878ab2e36db
IKEMEFUNA CHIDINMA <ikemefunachidinma123@gmail.com>
2026-03-14 15:11:30 +0100

    Add login to controller

--- src/main/java/com/example/frontendversioncontroltaskikemefunachidinma/bakery/ProductController.java
+++ src/main/java/com/example/frontendversioncontroltaskikemefunachidinma/bakery/ProductController.java
@@ -1,14 +1,21 @@
 package com.example.frontendversioncontroltaskikemefunachidinma.bakery;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
+import org.slf4j.Logger;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     @GetMapping
     public List<String> getProducts() {
+        logger.info("Fetching all bakery products");
         return List.of("Vanilla Cake", "Red Velvet", "Butter cream icing cupcakes", "Chocolate Chip Cookies", "Chocolate Cake");
     }
+
 }
