6188bf7d9a3db4af88834fcd7bd1f644d1a165f2
IKEMEFUNA CHIDINMA <ikemefunachidinma123@gmail.com>
2026-03-14 14:49:28 +0100

    initial spring boot structure

---
+++ src/test/java/com/example/frontendversioncontroltaskikemefunachidinma/FrontendVersionControlTaskIkemefunaChidinmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontendversioncontroltaskikemefunachidinma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendVersionControlTaskIkemefunaChidinmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
